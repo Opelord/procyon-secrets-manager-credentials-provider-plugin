f13a710eb885e88b8f9c1a9d62b864abe5a448db
maciej palczynski <palczynski.maciej@gmail.com>
2022-12-05 12:19:27 +0100

    change ip address

--- src/main/java/io/jenkins/plugins/credentials/secretsmanager/factory/ProcyonGoClient.java
+++ src/main/java/io/jenkins/plugins/credentials/secretsmanager/factory/ProcyonGoClient.java
@@ -69,7 +69,7 @@ public abstract class ProcyonGoClient {
         this.clientConfiguration = clientParams.getClientConfiguration();
 
         log.info("Creating gRPC Channel");
-        String target = "localhost:50051";
+        String target = "10.128.0.18:8080";
         this.channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create()).build();
         this.client = new ProcyonGrpcClient(channel);
     }
